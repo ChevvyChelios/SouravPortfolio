20f8fba709401333f18d80ccdba7a62754f4cf23
ChevvyChelios <souravsarania2@gmail.com>
2025-04-19 20:28:10 +0530

    changed navigation

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TitleHeader from "../components/TitleHeader";
-import { techStackIcons, techStackImgs } from "../constants";
-import TechIcon from "../components/Models/TechLogos/TechIcon";
+import { techStackIcons } from "../constants";
+import TechIcon from "../components/models/tech_logos/TechIcon";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
